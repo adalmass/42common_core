c2e2485de25e6a67ccbec8bfa71dc97ea5c70091
Alexandre Dalmasso <aldalmas@c1r3p9.42nice.fr>
2023-05-17 16:59:42 +0200

    all perfect

--- so_long/bonus/libft/ft_lstsize.c
+++ so_long/bonus/libft/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: aldalmas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:53:18 by aldalmas          #+#    #+#             */
-/*   Updated: 2023/05/16 11:05:51 by aldalmas         ###   ########.fr       */
+/*   Updated: 2023/05/17 14:51:38 by aldalmas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,27 +28,3 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (i);
 }
-
-unsigned char left = (octet << 4)
-unsigned char right = (octer >>4)
-return (left | right);
-
-return (octet >> 4 + octet << 4);
-
-unsigned char octet;
-
-int i 8;
-unsigned char bits;
-
-i > 0
-{
-	bits = (octet >> i & 1) + '0';
-	write (1 , &bits , 1);
-	i--;
-}
-
-i > 0
-ret = ret * 2 + (octet % 2)
-octet /=2;
-i--;
-
